da75728436a20c5344719d63fa41ffc11d773ab4
Carlos Romero <veracarlos66@hotmail.es>
2017-06-01 12:04:27 -0500

    jeje

---
+++ www/webroot/js/globals.js
@@ -0,0 +1,8 @@
+/*window.server = "https://taxiseguro2.local/php/";
+window.url = "https://taxiseguro2.local/";*/
+
+window.server = "http://192.168.1.79/taxiSeguro2/php/";
+window.url = "http://192.168.1.79/taxiSeguro2/";
+
+// window.server = "http://localhost/taxiSeguro2/php/";
+// window.url = "http://localhost/taxiSeguro2/";
