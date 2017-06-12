bb3dc0f5b1e4be440579c6df092cf5b975122708
Carlos Romero <veracarlos66@hotmail.es>
2017-06-12 10:47:56 -0500

    :ocean:

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,8 @@
 var platform;
 function onDeviceReady() {
 
+    grantedDevice = 0;
+
     platform = device.platform.toLowerCase();
     if(platform.match(/win/)){
         platform = "windows";
@@ -29,6 +31,7 @@ function onDeviceReady() {
                 switch(statuses[permission]){
                     case cordova.plugins.diagnostic.permissionStatus.GRANTED:
                         console.log("Permission granted to use "+permission);
+                        grantedDevice = 1;
                         break;
                     case cordova.plugins.diagnostic.permissionStatus.NOT_REQUESTED:
                         console.log("Permission to use "+permission+" has not been requested yet");
@@ -122,58 +125,19 @@ function onDeviceReady() {
 
 
 function checkState(){
-
-    cordova.plugins.diagnostic.requestRuntimePermissions(function(statuses){
-        for (var permission in statuses){
-            switch(statuses[permission]){
-                case cordova.plugins.diagnostic.permissionStatus.GRANTED:
-                    //alert("Permission granted to use "+permission);
-                    cordova.plugins.diagnostic.isLocationAvailable(function(available){
-                        // $('#state .location').addClass(available ? 'on' : 'off');
-
-                        //alert('isLocationAvailable');
-                        var info = (available ? 'on' : 'off');
-                        //alert(info);
-                        if (info == 'off') {
-                            alert('Porfavor, enciende tu GPS');
-                            var info = (available ? 'on' : 'off');
-                            //alert(info);
-                            if (info == 'off') {
-                                cordova.plugins.diagnostic.switchToLocationSettings();
-                            }
-                        }
-                    }, onError);
-                    break;
-                case cordova.plugins.diagnostic.permissionStatus.NOT_REQUESTED:
-                    alert("Permission to use "+permission+" has not been requested yet");
-                    break;
-                case cordova.plugins.diagnostic.permissionStatus.DENIED:
-                    alert("Permission denied to use "+permission+" - ask again?");
-                    break;
-                case cordova.plugins.diagnostic.permissionStatus.DENIED_ALWAYS:
-                    alert("Permission permanently denied to use "+permission+" - guess we won't be using it then!");
-                    break;
-            }
-        }
-    }, function(error){
-        alert("The following error occurred: "+error);
-    },[
-        cordova.plugins.diagnostic.permission.ACCESS_FINE_LOCATION,
-        cordova.plugins.diagnostic.permission.ACCESS_COARSE_LOCATION
-    ]);
     // Location
     var onGetLocationAuthorizationStatus;
-    // cordova.plugins.diagnostic.isLocationAvailable(function(available){
-    //     // $('#state .location').addClass(available ? 'on' : 'off');
+    cordova.plugins.diagnostic.isLocationAvailable(function(available){
+        // $('#state .location').addClass(available ? 'on' : 'off');
 
-    //     //alert('isLocationAvailable');
-    //     var info = (available ? 'on' : 'off');
-    //     //alert(info);
-    //     if (info == 'off') {
-    //         alert('Porfavor, enciende tu GPS');
-    //         cordova.plugins.diagnostic.switchToLocationSettings();
-    //     }
-    // }, onError);
+        //alert('isLocationAvailable');
+        var info = (available ? 'on' : 'off');
+        //alert(info);
+        if (info == 'off' && grantedDevice == 1) {
+            alert('Porfavor, enciende tu GPS');
+            cordova.plugins.diagnostic.switchToLocationSettings();
+        }
+    }, onError);
 
 
     if(platform === "ios"){
